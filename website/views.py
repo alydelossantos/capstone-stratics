248fd17612bfc0c0c4194e08c3942c3cc51a5dfa
abfayes <93405216+abfayes@users.noreply.github.com>
2021-10-31 23:33:19 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -3,6 +3,13 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
+
+@views.route('/home')
+@login_required
+def home():
+    image_file = url_for('static', filename='images/' + current_user.image_file)
+    return render_template("home.html", user= current_user, image_file = image_file)
+
 @views.route('/')
 def landing():
     return render_template("landing.html", user= current_user)
