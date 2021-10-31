1f7aa61b307b8d12d6c7ea63afcd7efb98f5ea21
abfayes <93405216+abfayes@users.noreply.github.com>
2021-10-31 23:40:14 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
 from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
