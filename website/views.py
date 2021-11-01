063dfc5892f2dcad0d7622b9c03987023e3a6193
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-01 09:02:27 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -3,12 +3,25 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
-
-@views.route('/home')
+@views.route('/home', methods=["GET", "POST"])
 @login_required
 def home():
+    if current_user.explore == "sample":
+        current_user.dname = "Sample Dataset"
+    else:
+        current_user.dname = "Customer Dataset"
+    print(current_user.dname)
     image_file = url_for('static', filename='images/' + current_user.image_file)
-    return render_template("home.html", user= current_user, image_file = image_file)
+    return render_template("home.html", user= current_user)
+
+@views.route('/home/explore-dataset', methods=["GET", "POST"])    
+@login_required
+def homeexp():
+    if request.method == 'POST':
+        current_user.explore = request.form['explore']
+        db.session.commit()
+    print(current_user.explore)
+    return redirect(url_for('views.home'))
 
 @views.route('/')
 def landing():
