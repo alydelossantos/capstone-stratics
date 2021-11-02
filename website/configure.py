6cbcd547c8029cc5892d8741a7fd30285ed53453
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-02 13:54:37 +0800

    Update configure.py

--- website/configure.py
+++ website/configure.py
@@ -1,1 +1,13 @@
 import os
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'horizonfeua@gmail.com'
+MAIL_PASSWORD = 'horizonfeu123'
+MAIL_DEFAULT_SENDER = 'horizonfeua@gmail.com'
+
+SECRET_KEY = 'asdfghjkl'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
