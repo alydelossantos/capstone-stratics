58d63c0e2c77f24d618bbbca0b878845b06afbfc
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-02 15:36:50 +0800

    Update command.py

--- website/command.py
+++ website/command.py
@@ -2,9 +2,7 @@ import click
 from flask import Flask
 from flask.cli import with_appcontext
 
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-db = SQLAlchemy(app)
+from .extensions import db
 from .models import User, Data, Strategies, Contact, Sampledata, Samplestrategies
 
 @click.command(name='create_tables')
