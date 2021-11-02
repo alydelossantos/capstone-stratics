43b2c1e64312a1f0ddf693c52df94cc8c53cf10e
Aly <84432568+alydelossantos@users.noreply.github.com>
2021-11-02 16:44:54 +0800

    Update configure.py

--- website/configure.py
+++ website/configure.py
@@ -9,5 +9,5 @@ MAIL_PASSWORD = 'horizonfeu123'
 MAIL_DEFAULT_SENDER = 'horizonfeua@gmail.com'
 
 SECRET_KEY = 'asdfghjkl'
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.getenv('postgres://jzyiaknneqredi:b3f16c49a8b520b2d627ba916908f41bc0a507f7cac2efcb23fa3a8947d76fa8@ec2-35-169-43-5.compute-1.amazonaws.com:5432/dc0chgkng9ougq')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
