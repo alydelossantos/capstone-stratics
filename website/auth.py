433d1fc92cbb92ff7dcd2cd6b3fcd0132546b931
deinsanconn <sancondeinielle@gmail.com>
2021-11-04 07:05:26 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -689,8 +689,8 @@ def deletestratcheck():
 def dashboard():
 
     cnx = create_engine("postgresql://jzyiaknneqredi:b3f16c49a8b520b2d627ba916908f41bc0a507f7cac2efcb23fa3a8947d76fa8@ec2-35-169-43-5.compute-1.amazonaws.com:5432/dc0chgkng9ougq", echo=True)
-    connn = cnx.connect()
-    df = pd.read_sql_table('sampledata', connn=cnx)
+    conn = cnx.connect()
+    df = pd.read_sql_table('sampledata', con=cnx)
     print(df)
 
     # independent variable
