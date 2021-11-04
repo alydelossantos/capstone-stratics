305887e8707e0c1ab3c82d33a0e1d4a4e213a871
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-04 10:22:01 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -33,14 +33,7 @@ def home():
         cnx = create_engine("postgresql://jzyiaknneqredi:b3f16c49a8b520b2d627ba916908f41bc0a507f7cac2efcb23fa3a8947d76fa8@ec2-35-169-43-5.compute-1.amazonaws.com:5432/dc0chgkng9ougq", echo=True)
         conn = cnx.connect()
         df = pd.read_sql_table('sampledata', con=cnx)
-    elif current_user.explore == "Customer Dataset":
-        current_user.dname = "Customer Dataset"
-        db.session.commit()
-    else:
-        current_user.dname = "Enter Dashboard Name"
-        db.session.commit()
-    print(current_user.dname)
-        # independent variable
+         # independent variable
         X = df.iloc[:,:-1].values
         X
 
@@ -156,6 +149,14 @@ def home():
         fig4 = go.Figure(data = data,layout = layout)
         fig4 = go.Figure(data = data,layout = layout)
         graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+    elif current_user.explore == "Customer Dataset":
+        current_user.dname = "Customer Dataset"
+        db.session.commit()
+    else:
+        current_user.dname = "Enter Dashboard Name"
+        db.session.commit()
+    print(current_user.dname)
+       
 
     image_file = url_for('static', filename='images/' + current_user.image_file)
     return render_template("home.html", user= current_user, image_file=image_file, graph1JSON=graph1JSON, 
