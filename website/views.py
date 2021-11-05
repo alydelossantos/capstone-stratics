c4bc08ee3bbd2015d02683ec3193240c1be4ea81
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-06 03:49:37 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -42,6 +42,113 @@ def home():
             df = pd.read_sql_table('data', con=cnx)
         else:
             flash("Add Records in Customer Management", category="error")
+        
+        # independent variable
+        X = df.iloc[:,:-1].values
+        X
+
+        # dependent variable - churn column
+        y = df.iloc[:,10]
+        y
+
+        # Counts number of null values - resulted that no values are missing.
+        null_columns=df.columns[df.isnull().any()]
+        df[null_columns].isnull().sum()
+
+        # Splitting Data into Train and Test
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+        print("X_train : ",X_train.shape)
+        print("X_test : ",X_test.shape)
+        print("y_train : ",y_train.shape)
+        print("y_test : ",y_test.shape)
+
+        # Outlier Detection
+        print(df.shape)
+        print(df.columns)
+
+        # Zscore
+        from scipy import stats
+        zscore = np.abs(stats.zscore(df['MonthlyCharges']))
+        print (zscore)
+
+        # zscore values higher than 3 are outliers.
+        threshold = 3
+        print(np.where(zscore >3))
+
+        df.corr(method='pearson')
+
+        # Create Pivot Table - compute for sum
+        pd.pivot_table(df, index=['State', 'InternetService'], aggfunc = 'sum')
+
+        # Create Pivot Table - compute for mean
+        pd.pivot_table(df, index=['State', 'InternetService'], aggfunc = 'mean')    
+
+        # Create Pivot Table - compute for count
+        pd.pivot_table(df, index=['State', 'InternetService'], aggfunc = 'count')
+
+        # Pie Chart
+        from plotly.offline import init_notebook_mode,iplot
+        import plotly.graph_objects as go
+        import cufflinks as cf
+        init_notebook_mode(connected=True)
+
+        #labels
+        lab = df["gender"].value_counts().keys().tolist()
+        #values
+        val = df["gender"].value_counts().values.tolist()
+        trace = go.Pie(labels=lab, 
+                        values=val, 
+                        marker=dict(colors=['red']), 
+                        # Seting values to 
+                        hoverinfo="value"
+                    )
+        data = [trace]
+
+        layout = go.Layout(title="Sex Distribution")
+        fig1 = go.Figure(data = data,layout = layout)
+        fig1.update_traces(hole=.4)
+        graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # Histogram - Service
+        # defining data
+        trace = go.Histogram(x=df['InternetService'],nbinsx=40,histnorm='percent')
+        data = [trace]
+        # defining layout
+        layout = go.Layout(title="Service Distribution")
+        # defining figure and plotting
+        fig2 = go.Figure(data = data,layout = layout)
+        graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # Histogram - State
+        # defining data
+        trace = go.Histogram(x=df['State'],nbinsx=52)
+        data = [trace]
+        # defining layout
+        layout = go.Layout(title="State")
+        # defining figure and plotting
+        fig3 = go.Figure(data = data,layout = layout)
+        fig3 = go.Figure(data = data,layout = layout)
+        graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+        # Histogram - Churn
+        # defining data
+        trace = go.Histogram(x=df['Churn'],nbinsx=3)
+        data = [trace]
+        # defining layout
+        layout = go.Layout(title="Churn Distribution")
+        # defining figure and plotting
+        fig4 = go.Figure(data = data,layout = layout)
+        fig4 = go.Figure(data = data,layout = layout)
+        graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+
+        image_file = url_for('static', filename='images/' + current_user.image_file)
+        return render_template("home.html", user= current_user, image_file=image_file, graph1JSON=graph1JSON, 
+        graph2JSON=graph2JSON, 
+        graph3JSON=graph3JSON,
+        graph4JSON=graph4JSON,)
+    
     else:
         current_user.dname = "Empty Dashboard"
     
