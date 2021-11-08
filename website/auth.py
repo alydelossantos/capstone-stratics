47e79b1a884fce2ee49dae090319cb35b2065931
shandonrm <84431808+shandonrm@users.noreply.github.com>
2021-11-09 01:08:41 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -789,5 +789,5 @@ def reset_token(token):
         user.password = form.password.data
         db.session.commit()
         flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('signin'))
+        return redirect(url_for('auth.signin'))
     return render_template('reset_token.html',title = 'Reset Password', form = form)
