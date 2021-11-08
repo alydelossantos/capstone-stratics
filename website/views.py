f8f32526bfd836e8d8c6046474a8497d587f22b8
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-09 06:09:26 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -450,8 +450,7 @@ def home():
             graph10JSON=graph10JSON,
             graph11JSON=graph11JSON,
             graph12JSON=graph12JSON,
-            graph13JSON=graph13JSON,
-             )
+            graph13JSON=graph13JSON  )
         elif current_user.explore == "customer":
             if db.session.query(Data).count() >=3 :
                 cnx = create_engine("postgresql://jzyiaknneqredi:b3f16c49a8b520b2d627ba916908f41bc0a507f7cac2efcb23fa3a8947d76fa8@ec2-35-169-43-5.compute-1.amazonaws.com:5432/dc0chgkng9ougq", echo=True)
