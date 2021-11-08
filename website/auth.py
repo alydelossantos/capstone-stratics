2449602d3531d3857c8618d9840f2f1ceb1a1ad5
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-08 23:02:31 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -749,7 +749,7 @@ def reset_request():
 @auth.route("/sign-in/reset-password/<token>",methods=['GET','POST'])
 def reset_token(token):
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('views.home'))
     user = User.verify_reset_token(token)
     if user is None:
         flash('Invalid or expired token','warning')
