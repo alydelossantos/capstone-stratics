312f755efaf838b66729f8d24f8d7eb186de6737
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-08 23:03:32 +0800

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,7 +1,6 @@
 from .extensions import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-from website.configure import SECRET_KEY
     
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
