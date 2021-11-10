a0e79f44a032eb55976185ee949437c8cb7cce7b
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-10 14:23:48 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -501,7 +501,7 @@ def home():
             #if db.session.query(Otherdata).join(User).filter(User.id == current_user.id).count() >=3 :
             cnx = create_engine("postgresql://jzyiaknneqredi:b3f16c49a8b520b2d627ba916908f41bc0a507f7cac2efcb23fa3a8947d76fa8@ec2-35-169-43-5.compute-1.amazonaws.com:5432/dc0chgkng9ougq", echo=True)
             conn = cnx.connect()
-            df = pd.read_sql_query('''SELECT user.id, otherdata.* FROM user LEFT OUTER JOIN otherdata ON user.id = otherdata.id''', con=cnx)
+            df = pd.read_sql_query('''SELECT user.id, otherdata.name FROM user LEFT OUTER JOIN otherdata ON user.id = otherdata.id''', con=cnx)
 
             # independent variable
             X = df.iloc[:,:1].values
