a98298320ef96bf04b4e7d19889ec5577cef5bdc
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-11 00:22:49 +0800

    Update command.py

--- website/command.py
+++ website/command.py
@@ -4,7 +4,7 @@ from flask.cli import with_appcontext
 
 from .extensions import db
 
-from .models import User, Data, Strategies, Contact, Sampledata, Otherdata, Otherstrategies, Task
+from .models import User, Data, Otherdata, Sampledata, Strategies, Otherstrategies, Samplestrategies, Contact, Task
 
 @click.command(name='create_tables')
 @with_appcontext
