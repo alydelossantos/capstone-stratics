c3d4dae190469800ac9ffe6baf24dea3437f692a
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-14 21:23:12 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -43,7 +43,7 @@ def home():
     conn = cnx.connect()
     if current_user.explore == "sample":
         total = db.session.query(Sampledata).count()
-        avg = 1899
+        avg = db.session.query(func.sum(SampleData.MonthlyCharges)).all()
         df = pd.read_sql_table('sampledata', con=cnx)
 
         # Label Encoder
