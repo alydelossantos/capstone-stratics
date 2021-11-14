e7be49d711fe4756db1544b301964a6480855d9a
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-15 04:56:32 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -931,8 +931,8 @@ def churnanalytics():
 
         for i in range(len(Xnew)):
             kctn['Churn Probability'][i] = proba[i]
-            if i >= 1:
-                predd = kctn[['account_no','Churn Probability']].values.tolist()
+            if i <= len(Xnew):
+                predd = kctn[['account_no', 'amount_paid', 'monthly','Churn Probability']].values.tolist()
         image_file = url_for('static', filename='images/' + current_user.image_file)
         return render_template("churn-analysis.html", user= current_user, image_file=image_file, my_list=predd)
     image_file = url_for('static', filename='images/' + current_user.image_file)
