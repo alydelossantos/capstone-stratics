98887ec526e88514335722badf08c41598b5ed4a
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-14 22:43:20 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -43,7 +43,7 @@ def home():
     conn = cnx.connect()
     if current_user.explore == "sample":
         total = db.session.query(Sampledata).count()
-        ave = db.session.query(func.avg(Sampledata.MonthlyCharges).label("average"))
+        ave = db.session.query(db.func.avg(Sampledata.MonthlyCharges).label("average")).all()
         avg = ave
         df = pd.read_sql_table('sampledata', con=cnx)
 
