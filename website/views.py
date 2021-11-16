a52154513a73a5697acc1b48b78500f733f0fb1a
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-17 01:03:50 +0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -851,10 +851,10 @@ def churnanalytics():
         if current_user.cname == "Kalibo Cable":
             active = Data \
                 .query \
-                .filter(Data.disconnection_date == "").count()
+                .filter(Data.disconnection_date == None).count()
             disconnected = Data \
                 .query \
-                .filter(Data.disconnection_date != "").count()
+                .filter(Data.disconnection_date != None).count()
             rate = round(((disconnected/active) * 100), 2)
             kctn = pd.read_sql_table('data', con=cnx)
             kctn.head()
