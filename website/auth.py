d8c53aeb9c3f63d7a9b2d6149ec7cd5576c99362
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-26 02:25:06 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -407,6 +407,16 @@ def importcsv():
             db.session.commit()
             
             flash("CSV File Added Successfully")
+            col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid',
+                   'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
+            records = pd.read_csv("https://github.com/alydelossantos/capstone-stratics/tree/main/website/static/file" + current_user.csv, names=col, header=0)
+            
+            for i, row in records.iterrows():
+                sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
+                values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                        row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
+            db.session.execute(sql, values, if_exists='append')
+            db.session.commit()
             
             return redirect(url_for('auth.custman'))
         return redirect(url_for('auth.custman'))
