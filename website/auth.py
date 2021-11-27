536d9760d34c50265822be23d1da6a60e8a86069
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-28 03:48:42 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -413,7 +413,7 @@ def importcsv():
             
             col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid',
                    'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
-            records = pd.read_csv("https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/" + current_user.csv + "?token=AUEFNOB4EEOP35TGMANFKTLBUJXAQ", names=col, header=0)
+            records = pd.read_csv(r"https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/" + current_user.csv, names=col, header=0)
             
             for i, row in records.iterrows():
                 sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
