e1d517b7bfeb1a587c79bf495b5f3e247641121b
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-28 22:21:42 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -413,8 +413,8 @@ def importcsv():
             
             col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid',
                    'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
-            CSV_FILE = current_user.csv
-            records = pd.read_csv("https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv?token=AUEFNOCPKCT5HB3MO7SMGKDBUKZTE", names=col, header=0)
+            CSV_FILE = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv?token=AUEFNOCPKCT5HB3MO7SMGKDBUKZTE"
+            records = pd.read_csv(CSV_FILE, names=col, header=0)
             
             for i, row in records.iterrows():
                 sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
