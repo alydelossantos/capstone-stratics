d4a79a9f6fad0eb0eb2cbf7bf0905a8c93f74423
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-28 23:42:15 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -423,7 +423,7 @@ def importcsv():
                 sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
                 values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
                         row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
-            conn.cursor().execute(sql, values, if_exists='append')
+            conn.execute(sql, values, if_exists='append')
             conn.commit()
             
             db.session.commit()
