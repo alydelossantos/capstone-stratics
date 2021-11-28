99335f80e15560f26f6c799ede715fdb8681c9d3
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 01:50:35 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -45,7 +45,7 @@ knotel = "Kalibo Cable"
 knocable = "Kalibo"
 abbrenoinc = "KCTN"
 
-conn = psycopg2.connect("postgresql://ympxkbvvsaslrc:45cc51f6a20ea1519edcb35bd69cfdfda91968a390ef9fb2291fb8f3c020cf58@ec2-54-160-35-196.compute-1.amazonaws.com:5432/dd3k0hhqki80nh", echo=True)
+conn = psycopg2.connect("postgresql://ympxkbvvsaslrc:45cc51f6a20ea1519edcb35bd69cfdfda91968a390ef9fb2291fb8f3c020cf58@ec2-54-160-35-196.compute-1.amazonaws.com:5432/dd3k0hhqki80nh")
 #conn = cnx.connect()
 cur = conn.cursor()
 
