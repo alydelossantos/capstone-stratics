ad03c1d2e9b1a11b3b982ad0c62bba240d15ae6c
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 00:17:11 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -408,7 +408,7 @@ def importcsv():
     if current_user.cname == "Kalibo Cable":
         if request.method == 'POST':
             if request.files['csv']:
-                csv_file = request.files['csv']
+                csv_file = save_file(request.files['csv'])
                 current_user.csv = csv_file  
             db.session.commit()
             flash("CSV File Added Successfully")
