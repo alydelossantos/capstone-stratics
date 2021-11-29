1da207c208b94d859a5c61c3be69a8c2b39b976e
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 23:33:28 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -423,9 +423,10 @@ def importcsv():
         CSV_FILE = requests.get(url).text
         records = pd.read_csv(url, names=col, header=0)
         print(records)
-        for row in records.iterrows():
+        for i, row in records.iterrows():
             sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
-            values = (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18])
+            values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                    row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
             try:
                 cur.execute(sql, values)
                 conn.commit()
