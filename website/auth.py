782ed0f65f19ac0cbf0d56853801ba5dd7413e2e
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 22:25:57 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 import string
-import io 
+from io import StringIO 
 import requests
 import psycopg2
 import csv
@@ -414,11 +414,11 @@ def importcsv():
                 csv_file = save_import(request.files['csv'])
                 current_user.csv = csv_file  
             db.session.commit()
-            token = '?token=AWIUAIGKURRUPXEETCSOFADBUPRVM'
+            #token = '?token=AWIUAIGKURRUPXEETCSOFADBUPRVM'
             col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid', 'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
             url = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv?token=AWIUAIBWGPTZN6OYMESU6LDBUTN7Y"          
             CSV_FILE = requests.get(url).text
-            records = pd.read_csv(io.StringIO(CSV_FILE), names=col, header=0)
+            records = pd.read_csv(StringIO(CSV_FILE), names=col, header=0)
             records.head()
             for row in records.iterrows():
                 sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
