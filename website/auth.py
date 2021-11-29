78048b1d41d4026123f0f35ebea2f8af47ff83b8
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-30 01:35:49 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -418,22 +418,27 @@ def importcsv():
             #flash("CSV File Added Successfully")
             #return redirect(url_for('auth.custman'))
         #token = '?token=AWIUAIGKURRUPXEETCSOFADBUPRVM'
+        
         col = ['id','account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid', 'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
         url = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv"          
         CSV_FILE = requests.get(url).text
         records = pd.read_csv(url, names=col, header=0)
         print(records)
-        for i, row in records.iterrows():
-            sql = '''INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)'''
-            values = (row['id'], row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
-                    row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
+        with open(url, 'r') as csvfile:
+            next(csvfile)
+            
+        #for i, row in records.iterrows():
+            #sql = '''INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)'''
+            #values = (row['id'], row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                    #row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
             try:
-                cur.execute(sql, values, if_exists='append')
+                #cur.execute(sql, values, if_exists='append')
+                cur.copy_from(csvfile, 'data', sep=',')
                 conn.commit()
             except:
                 conn.rollback()
             #conn.close()
-            print(i, row['account_no'])
+            #print(i, row['account_no'])
 
         #col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid', 'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
 
