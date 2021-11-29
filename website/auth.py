e8e8b22b57addecbb4ab9ad2c2fcc0d634775d0c
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 19:11:25 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -419,8 +419,8 @@ def importcsv():
             CSV_FILE = requests.get(url).text
             records = pd.read_csv(io.StringIO(CSV_FILE), header=0)
             for row in records.iterrows():
-                sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
-                values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                sql = "INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
+                values = (row['id']=276, row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
                         row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
                 try:
                     cur.execute(sql, values, if_exists='append')
