a38a16606682a896d6343602cf47808a8177dea1
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-29 18:34:06 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -414,22 +414,21 @@ def importcsv():
                 csv_file = save_import(request.files['csv'])
                 current_user.csv = csv_file  
             db.session.commit()
-            for f in os.listdir('https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/'):
-                print(f)
-            '''
-            with open(url_for('static', filename='file/' + 'kalibo2018.csv'), newline='', encoding='utf8') as csvfile:
-                records =csv.reader(csvfile)
-                for row in records:
-                    sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
-                    values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
-                            row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
-                    try:
-                        cur.execute(sql, values, if_exists='append')
-                        conn.commit()
-                    except:
-                        conn.rollback()
+            
+            url = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv?token=AWIUAIGKURRUPXEETCSOFADBUPRVM"
+            CSV_FILE = requests.get(url).content
+            records = pd.read_csv(StringIO(CSV_FILE), header=0)
+            for row in records:
+                sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"
+                values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                        row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
+                try:
+                    cur.execute(sql, values, if_exists='append')
+                    conn.commit()
+                except:
+                    conn.rollback()
                 conn.close()
-            '''
+
             '''
             col = ['account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid',
                    'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
