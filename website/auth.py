bb7e2fe912ae9555be29be1975c63a80b4edf199
abfayes <93405216+abfayes@users.noreply.github.com>
2021-12-01 00:39:01 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -425,7 +425,10 @@ def importcsv():
             print(records['last_modified_on'])
             for i, row in records.iterrows():
                 print(i)
-
+                row = Data.query.count()
+                count = Data.query.filter(Data.id >= row).count()
+                if count >= 1:
+                    id = row + count
                 sql = "INSERT INTO data (account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
                 values = (row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'], row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
  
