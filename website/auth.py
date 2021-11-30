8cd77ad7f92832343dcad6142266323f17a691b7
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-30 12:28:24 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -416,22 +416,23 @@ def importcsv():
             db.session.commit()
 
         #col = ['id','account_no', 'subscriber', 'address', 'zone', 'services', 'monthly', 'collector', 'status', 'amount_paid', 'total_paid', 'ref_no', 'date_paid', 'category', 'activation_date', 'disconnection_date', 'reactivation_date', 'last_modified_on', 'churn']
-        url = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv"          
-        records = pd.read_csv(url)
-        print(records)
+            url = "https://raw.githubusercontent.com/alydelossantos/capstone-stratics/main/website/static/file/kalibo2018.csv"          
+            records = pd.read_csv(url)
+            print(records)
+            flash("CSV File Added Successfully")
+            return redirect(url_for('auth.custman'))
 
-        for i, row in records.iterrows():
-            sql = '''INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)'''
-            values = (row['id'], row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
-                    row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
-            try:
-                cur.execute(sql, values, if_exists='append')
-                cur.execute('''SELECT * FROM data''')
+        #for i, row in records.iterrows():
+            #sql = '''INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)'''
+            #values = (row['id'], row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'],
+                    #row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
+            #try:
+                #cur.execute(sql, values, if_exists='append')
+                #cur.execute('''SELECT * FROM data''')
                 #cur.copy_from(readcsv, 'data', sep=',')
-                conn.commit()
-            except:
-                conn.rollback()
-        flash("CSV File Added Successfully")
+                #conn.commit()
+            #except:
+                #conn.rollback()
 
         return redirect(url_for('auth.custman'))
     else:
