754a27067eb7648b118309b98c6a42827ae9405c
abfayes <93405216+abfayes@users.noreply.github.com>
2021-11-30 14:27:01 +0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -421,20 +421,18 @@ def importcsv():
             print(records)
 
             for i, row in records.iterrows():
-                sql = "INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
+                sql = "INSERT INTO data (id, account_no, subscriber, address, zone, services, monthly, collector, status, amount_paid, total_paid, ref_no, date_paid, category, activation_date, disconnection_date, reactivation_date, last_modified_on, churn) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"
                 values = (row['id'], row['account_no'], row['subscriber'], row['address'], row['zone'], row['services'], row['monthly'], row['collector'], row['status'], row['amount_paid'], row['total_paid'], row['ref_no'], row['date_paid'], row['category'], row['activation_date'], row['disconnection_date'], row['reactivation_date'], row['last_modified_on'], row['churn'])
             try:
-                cur.execute(sql, values, if_exists='append')
-                cur.execute('SELECT * FROM data')
+                cur.execute(sql, values, row, if_exists='append')
+                cur.execute('''SELECT * FROM data;''')
                 #cur.copy_from(readcsv, 'data', sep=',')
                 conn.commit()
             except:
                 conn.rollback()
             flash("CSV File Added Successfully")
             return redirect(url_for('auth.custman'))
-        cur.execute('SELECT * FROM data')
-        #cur.copy_from(readcsv, 'data', sep=',')
-        conn.commit()
+
         return redirect(url_for('auth.custman'))
     else:
         sd = Otherdata \
